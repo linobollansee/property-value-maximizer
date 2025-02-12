987f9770bbc70a59f08c72ca081ac160f564a554
linobollansee <linobollansee@hotmail.com>
2025-02-12 17:56:32 +0000

    evaluate_reg.py new metrics

--- src/machine_learning/evaluate_reg.py
+++ src/machine_learning/evaluate_reg.py
@@ -3,7 +3,9 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import streamlit as st
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.metrics import mean_absolute_error, mean_squared_error, \
+                            r2_score, explained_variance_score, \
+                            median_absolute_error
 
 
 def regression_performance(X_train, y_train, X_test, y_test, pipeline):
@@ -18,12 +20,22 @@ def regression_evaluation(X, y, pipeline):
     prediction = pipeline.predict(X)
     if isinstance(y, pd.DataFrame):
         y = y['SalePrice'].values
+
+    # Metrics
     mae = mean_absolute_error(y, prediction)
     r2 = r2_score(y, prediction)
     mse = mean_squared_error(y, prediction)
     rmse = np.sqrt(mse)
+    mape = np.mean(np.abs((y - prediction) / y)) * 100
+    evs = explained_variance_score(y, prediction)
+    medae = median_absolute_error(y, prediction)
+    
+    # Displaying all metrics
     st.write('Mean Absolute Error:', round(mae, 3))
     st.write('R2 Score:', round(r2, 3))
     st.write('Mean Squared Error:', round(mse, 3))
     st.write('Root Mean Squared Error:', round(rmse, 3))
+    st.write('Mean Absolute Percentage Error:', round(mape, 3))
+    st.write('Explained Variance Score:', round(evs, 3))
+    st.write('Median Absolute Error:', round(medae, 3))
     st.write("\n")
