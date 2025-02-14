7a9e0883ef3de2a192b3e02ac05c6edc2e845851
linobollansee <linobollansee@hotmail.com>
2025-02-14 01:59:57 +0000

    data_management.py ordering, docstrings

--- src/data_management.py
+++ src/data_management.py
@@ -4,17 +4,37 @@ import pandas as pd
 import streamlit as st
 
 
+# Caching ensures data is not reloaded unnecessarily in Streamlit
 @st.cache_data
 def load_ames_data():
-    df = pd.read_csv("outputs/datasets/collection/HousePricesRecords.csv")
-    return df
+    """
+    Load the Ames housing dataset from a CSV file.
 
-
-def load_pkl_file(file_path):
-    return joblib.load(filename=file_path)
+    Returns:
+        pd.DataFrame: A DataFrame containing the Ames housing dataset.
+    """
+    return pd.read_csv("outputs/datasets/collection/HousePricesRecords.csv")
 
 
 @st.cache_data
 def load_inherited_houses_data():
-    df = pd.read_csv("outputs/datasets/collection/InheritedHouses.csv")
-    return df
+    """
+    Load the inherited houses dataset from a CSV file.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the inherited houses dataset.
+    """
+    return pd.read_csv("outputs/datasets/collection/InheritedHouses.csv")
+
+
+def load_pkl_file(file_path):
+    """
+    Load a serialized object from a pickle (.pkl) file.
+
+    Args:
+        file_path (str): The path to the pickle file.
+
+    Returns:
+        object: The deserialized object from the pickle file.
+    """
+    return joblib.load(filename=file_path)
